23a0acea00df493c8dffe9bce7cf330c927f005a
yuanwang <me@yuanwang.ca>
2021-03-13 15:47:54 -0600

    add basic servent api type

---
+++ src/ApiType.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TypeOperators #-}
+
+module ApiType where
+
+import Data.Aeson
+import Data.Aeson.Types
+import Data.Proxy ()
+import GHC.Generics
+import Servant
+
+data Scientist
+  = Scientist
+      { sId :: Int,
+        sName :: String
+      }
+  deriving (Eq, Show, Generic)
+
+instance ToJSON Scientist
+
+scientists :: [Scientist]
+scientists =
+  [ Scientist 1 "Isaac Newton",
+    Scientist 2 "Albert Einstein",
+    Scientist 3 "Gottfried Wilhelm Leibniz",
+    Scientist 4 "Stephen Hawking",
+    Scientist 5 "Pythagoras",
+    Scientist 6 "Wernher Von Braun"
+  ]
+
+type API =
+  "scientist" :> Get '[JSON] [Scientist]
+    :<|> Raw
+
+server :: Server API
+server =
+  pure scientists
+    :<|> serveDirectoryFileServer "dist"
+
+myAPI :: Proxy API
+myAPI = Proxy
+
+app :: Application
+app = serve myAPI server
