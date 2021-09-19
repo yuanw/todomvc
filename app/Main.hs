529853c1b88068aa44442d1b0e6ab9625fcaea2d
yuanwang <yuan.wang@workiva.com>
2021-09-19 15:20:39 -0600

    apply hlint

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import RIO
+import Control.Monad ((<=<))
 import Data.Maybe (fromMaybe)
 import Network.Wai.Handler.Warp (run)
 import Server (Env (..), app)
@@ -10,11 +11,11 @@ import Prelude (read)
 main :: IO ()
 main = do
   ref <- newTVarIO []
-  pStr <- fromMaybe "8080" <$> lookupEnv "PORT"
+  port <- fmap (fromMaybe 8080) $ fmap join    $ (fmap . fmap) readMaybe $  lookupEnv "PORT"
   staticFilePath <- fromMaybe "/var/www" <$> lookupEnv "STATIC_FILE_PATH"
   let env =
         Env
-          { appPort = read pStr :: Int,
+          { appPort = port,
             staticDir = staticFilePath,
             scientistStore = ref
           }
