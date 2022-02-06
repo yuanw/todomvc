ca15a3e3e34d3e8aac08900f47aaddfdbd5fdd26
yuanw <me@yuanwang.ca>
2022-02-06 14:50:06 -0600

    update

--- script/push_image.sh
+++ script/push_image.sh
@@ -1,10 +1,8 @@
-#! /usr/bin/env nix-shell
-#! nix-shell -i bash -p skopeo
-
 set -eu
 
-OCI_ARCHIVE=$(nix-build --no-out-link)
-DOCKER_REPOSITORY="docker://gcr.io/${GOOGLE_CLOUD_PROJECT_NAME}/${GOOGLE_CLOUD_RUN_SERVICE_NAME}:${GITHUB_SHA}"
 
-skopeo login -u _json_key -p ${DOCKER_ACCESS_TOKEN}  gcr.io 2>/dev/null
-skopeo copy "docker-archive:${OCI_ARCHIVE}" "$DOCKER_REPOSITORY"
+OCI_ARCHIVE=$(nix-build --no-out-link)
+DOCKER_REPOSITORY="docker://gcr.io/$GOOGLE_CLOUD_PROJECT_NAME/$GOOGLE_CLOUD_RUN_SERVICE_NAME:$GITHUB_SHA"
+
+skopeo login -u _json_key -p "${GCR_DEVOPS_SERVICE_ACCOUNT_KEY}"  gcr.io
+skopeo copy "docker-archive:$OCI_ARCHIVE" "$DOCKER_REPOSITORY"
